ac7d8749c690c85d0766ab072bf594b192bc3483
Thiri <thirihtet246@gmail.com>
2023-08-27 16:56:20 +0630

    Add cv styling

--- src/pages/home/cv/Cv.jsx
+++ src/pages/home/cv/Cv.jsx
@@ -2,16 +2,19 @@ import PropTypes from 'prop-types';
 import Navigation from "../../../components/Navigation"
 import Button from "../../../components/Button"
 import cv from "../../../assets/img/cv.png"
-import moon from "../../../assets/img/moon.png"
+import moon from "../../../assets/img/moon2.svg"
+import './Cv.css'
 
 const Cv = ({type}) => {
   return (
-    <section>
+    <section className='cv'>
     <Navigation section='cv' type={type}/>
       <h1>CV</h1>
-      <img src={cv} alt='cv'/>
-      <Button name='Get CV' link ='https://drive.google.com/file/d/1hpZ8Jhfe8rqFtprV3lQgck5dfVTES6Fs/view?usp=sharing'/>
-      <img src={moon} className='hero-moon' alt='moon' />
+      <div className='cv-body'>
+      <img src={cv} className='cv-img' alt='cv'/>
+        <Button name='Get CV' link ='https://drive.google.com/file/d/1hpZ8Jhfe8rqFtprV3lQgck5dfVTES6Fs/view?usp=sharing'/>
+        <img src={moon} className='cv-moon p-abs' alt='cv-moon' />
+      </div>
     </section>
   )
 }
