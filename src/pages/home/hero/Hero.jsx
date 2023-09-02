cd440dd42249f21dec0f82d28406b3a4d0ec9184
Thiri <thirihtet246@gmail.com>
2023-09-02 14:02:43 +0630

    Add home page ani

--- src/pages/home/hero/Hero.jsx
+++ src/pages/home/hero/Hero.jsx
@@ -2,22 +2,43 @@ import Navigation from "../../../components/Navigation"
 import moon from  "../../../assets/img/moon.png"
 import mars from "../../../assets/img/mars.svg"
 import style from './Hero.module.css'
+import { useRef } from "react"
+import { useInView } from "framer-motion"
 
 const Hero = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
+  const textStyle = {
+    opacity: isInView ? 1 : 0,
+    transition: "all 2s ease-in-out 0.5s"
+  }
+
+  
+  const marsStyle = {
+    transform: isInView ? "translate(0%)" : "translate(50%,-50%)",
+    transition: "all 1s ease-in-out 0.5s"
+  }
+
+  
+  const moonStyle = {
+    transform: isInView ? "translate(-50%, 0%)" : "translate(-50%,50%)",
+    transition: "all 1s ease-in-out 0.5s"
+  }
   
   
   return (
-    <section id='hero' className={`grid-center ${style.hero}`}>
+    <section ref={ref} id='hero' className={`grid-center ${style.hero}`}>
       <Navigation section="hero" className={`p-abs ${style.nav}`}/>
-        <p className={`f-color h-font ${style['greet-text']}`}>
+        <p className={`f-color h-font ${style['greet-text']}`} style={textStyle}>
           Hey there,
           <br/>
           <span className={style.name}>I&apos;m Grace.</span>
           <br/>
           Turn your dream website into a stunning reality.
         </p>
-      <img src={moon} className={`p-abs ${style['hero-moon']} planet`} alt='moon' />
-      <img src={mars} className={`p-abs ${style['hero-mars']} planet`} alt='mars' />
+      <img src={moon} className={`p-abs ${style['hero-moon']} planet`} style={moonStyle} alt='moon' />
+      <img src={mars} className={`p-abs ${style['hero-mars']} planet`} style={marsStyle} alt='mars' />
     </section>
   )
 }
