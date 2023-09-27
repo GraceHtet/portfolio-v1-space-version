9f5d1f643c66d997ec9074fcf3cd04de9eb2a9b8
Thiri <thirihtet246@gmail.com>
2023-09-27 14:51:30 +0630

    Update CV page animation

--- src/pages/home/cv/Cv.jsx
+++ src/pages/home/cv/Cv.jsx
@@ -28,7 +28,7 @@ const Cv = ({type}) => {
       <h1>CV</h1>
       <div className='cv-body' style={fadeIn}>
       <img src={cv} className='cv-img' alt='cv'/>
-        <Button name='Get CV' link ='https://drive.google.com/file/d/1hpZ8Jhfe8rqFtprV3lQgck5dfVTES6Fs/view?usp=sharing'/>
+        <Button name='Get CV' link ='https://drive.google.com/file/d/1H0tgnl-FarJkRcrGvecGBv88uc5zXwKR/view?usp=sharing'/>
         <img src={moon} className='cv-moon p-abs planet' style={fadeUp} alt='cv-moon' />
       </div>
     </section>
